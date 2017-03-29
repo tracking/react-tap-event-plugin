5d3b4ae43deaef041aab22a30c925d2a7e20be40
tracking <ysii@vip.qq.com>
2017-03-29 13:35:34 +0800

    Update TapEventPlugin.js
    增加tapMoveThreshold配置

--- src/TapEventPlugin.js
+++ src/TapEventPlugin.js
@@ -46,7 +46,6 @@ var isTouch = function(topLevelType) {
  * Number of pixels that are tolerated in between a `touchStart` and `touchEnd`
  * in order to still be considered a 'tap' event.
  */
-var tapMoveThreshold = 10;
 var ignoreMouseThreshold = 750;
 var startCoords = {x: null, y: null};
 var lastTouchEvent = null;
@@ -109,7 +108,10 @@ var now = (function() {
   }
 })();
 
-function createTapEventPlugin(shouldRejectClick) {
+function createTapEventPlugin(options) {
+  var shouldRejectClick = options.shouldRejectClick;
+  var tapMoveThreshold = options.tapMoveThreshold;
+  
   return {
 
     tapMoveThreshold: tapMoveThreshold,
